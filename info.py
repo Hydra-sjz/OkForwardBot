3ca42bd09e5a1d68336117f15619061973ee9769
Hansaka Anuhas <121796740+Hansaka-Anuhas@users.noreply.github.com>
2023-04-30 20:48:18 +0530

    Create info.py

---
+++ info.py
@@ -0,0 +1,5 @@
+from os import environ
+
+API_ID = int(environ.get('API_ID', '13570748'))
+API_HASH = environ.get('API_HASH', '5e7dcf76a539a41177fb5b44f767d069')
+BOT_TOKEN = environ.get('BOT_TOKEN', '5904537043:AAHn-_s3R9DXoluENTf1ML_pumqcDckriMw')
