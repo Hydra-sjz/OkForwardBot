7703c1aaf5ed55703807457bf646bbc533de0435
Hansaka Anuhas <121796740+Hansaka-Anuhas@users.noreply.github.com>
2023-04-30 20:49:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,6 @@ logging.getLogger("pyrogram").setLevel(logging.ERROR)
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 from info import API_ID, API_HASH, BOT_TOKEN
-from utils import temp
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
 
