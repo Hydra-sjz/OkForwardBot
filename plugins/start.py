f9cd1bf7b827662b91a6b0a2ab51d7cba2bd5568
Hansaka Anuhas <121796740+Hansaka-Anuhas@users.noreply.github.com>
2023-05-01 16:22:52 +0530

    /id

--- plugins/start.py
+++ plugins/start.py
@@ -19,3 +19,50 @@ Forward your source channel message to this bot. If source channel is forward re
 /set_skip - Set skip message.
 /set_channel - Set target channel."""
     await message.reply(text, reply_markup=InlineKeyboardMarkup(btn))
+
+
+@Client.on_message(filters.command('id'))
+async def showid(client, message):
+    chat_type = message.chat.type
+    if chat_type == enums.ChatType.PRIVATE:
+        user_id = message.chat.id
+        first = message.from_user.first_name
+        last = message.from_user.last_name or "None"
+        username = f"@{message.from_user.username}" or "None"
+        await message.reply_text(
+            f"★ First Name: {first}\n★ Last Name: {last}\n★ Username: {username}\n★ User ID: <code>{user_id}</code>",
+            quote=True
+        )
+
+    elif chat_type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
+        _id = ""
+        _id += (
+            "★ Chat ID: "
+            f"<code>{message.chat.id}</code>\n"
+        )
+        if message.reply_to_message:
+            _id += (
+                "★ User ID: "
+                f"<code>{message.from_user.id if message.from_user else 'Anonymous'}</code>\n"
+                "★ Replied User ID: "
+                f"<code>{message.reply_to_message.from_user.id if message.reply_to_message.from_user else 'Anonymous'}</code>\n"
+            )
+            file_info = get_file_id(message.reply_to_message)
+        else:
+            _id += (
+                "★ User ID: "
+                f"<code>{message.from_user.id if message.from_user else 'Anonymous'}</code>\n"
+            )
+            file_info = get_file_id(message)
+        if file_info:
+            _id += (
+                f"★ {file_info.message_type}: "
+                f"<code>{file_info.file_id}</code>\n"
+            )
+        await message.reply_text(
+            _id,
+            quote=True
+        )
+
+    elif chat_type == enums.ChatType.CHANNEL:
+        await message.reply_text(f'★ Channel ID: <code>{message.chat.id}</code>')
