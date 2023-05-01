fdd67dcf6552dc42138915dc5c6445a161a1ff52
Hansaka Anuhas <121796740+Hansaka-Anuhas@users.noreply.github.com>
2023-05-01 11:45:30 +0530

    Update allowed.py

--- plugins/allowed.py
+++ plugins/allowed.py
@@ -1,11 +1,11 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from info import IS_PUBLIC, ADMINS
+from info import IS_PUBLIC_BOT, ADMINS
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
 async def admins(_, client, message: Message):
-    if IS_PUBLIC:
+    if IS_PUBLIC_BOT:
         return message.from_user.id not in ADMINS
 
 allowed_users = filters.create(admins)
