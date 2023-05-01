7622f8cf9326ef2ae5d9beae362d09737636c9ca
Hansaka Anuhas <121796740+Hansaka-Anuhas@users.noreply.github.com>
2023-05-01 10:10:29 +0530

    🤒

--- plugins/forward.py
+++ plugins/forward.py
@@ -129,7 +129,7 @@ async def forward_files(lst_msg_id, chat, msg, bot, user_id):
 
     async with lock:
         try:
-            async for message in bot.iter_messages(chat, lst_msg_id, CURRENT.get(user_id)):
+            async for message in bot.iter_messages(chat, lst_msg_id, CURRENT.get(user_id) if CURRENT.get(user_id) else 0):
                 if CANCEL.get(user_id):
                     await msg.edit(f"Successfully Forward Canceled!")
                     break
