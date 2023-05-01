e15df39e05ea39c6b19106fd633b38872f960102
Hansaka Anuhas <121796740+Hansaka-Anuhas@users.noreply.github.com>
2023-05-01 11:41:13 +0530

    done

---
+++ plugins/allowed.py
@@ -0,0 +1,18 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from info import ALLOWED, ADMINS
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+async def admins(_, client, message: Message):
+    if ALLOWED:
+        return message.from_user.id not in ADMINS
+
+allowed_users = filters.create(admins)
+
+@Client.on_message(filters.private & allowed_users & filters.incoming)
+async def not_admins(bot, message):
+    btn = [[
+        InlineKeyboardButton('Repo', url='https://github.com/Hansaka-Anuhas/ForwardBot')
+    ]]
+    await message.reply("You can't access this bot, Create your own bot", reply_markup=InlineKeyboardMarkup(btn))
