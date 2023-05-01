67fc1e1b9505b17e19511c5db40e3e83e86a40ba
Hansaka Anuhas <121796740+Hansaka-Anuhas@users.noreply.github.com>
2023-05-01 17:10:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -3,6 +3,6 @@ from os import environ
 API_ID = int(environ.get('API_ID', ''))
 API_HASH = environ.get('API_HASH', '')
 BOT_TOKEN = environ.get('BOT_TOKEN', '')
-CAPTION = environ.get('CAPTION', '<code>{file_name}</code>')
+FILE_CAPTION = environ.get('CAPTION', '<code>{file_name}</code>')
 OWNER = int(environ.get('OWNER', '5493832202'))
 PRIVATE_BOT = bool(environ.get('PRIVATE_BOT', False))
