4bece7b10704c53e71657a26afbda33529fb484a
Hansaka Anuhas <121796740+Hansaka-Anuhas@users.noreply.github.com>
2023-05-01 11:53:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -5,7 +5,7 @@ id_pattern = re.compile(r'^.\d+$')
 
 API_ID = int(environ.get('API_ID', '13570748'))
 API_HASH = environ.get('API_HASH', '5e7dcf76a539a41177fb5b44f767d069')
-BOT_TOKEN = environ.get('BOT_TOKEN', '5904537043:AAHn-_s3R9DXoluENTf1ML_pumqcDckriMw')
+BOT_TOKEN = environ.get('BOT_TOKEN', '5899045489:AAG8XtuFD1rbkS0dlnFLOzk9molalCAM1C8')
 CAPTION = environ.get('CAPTION', '<code>{file_name}</code>')
 ADMINS = [int(admins) if id_pattern.search(admins) else admins for admins in environ.get('ADMINS', '5493832202').split()]
 PRIVATE_BOT = bool(environ.get('PRIVATE_BOT', False))
