4019c78867d2776734f0ed507892eb1975991bc6
Hydra <119828208+Hydra-sjz@users.noreply.github.com>
2023-05-05 11:04:09 +0530

    Create hellofly.py

---
+++ hellofly.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
