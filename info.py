7ed0f0f8af86e061a8f6eb61154c496db54c7876
Hydra <119828208+Hydra-sjz@users.noreply.github.com>
2023-06-10 15:46:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,9 +1,9 @@
 from os import environ
 
-API_ID = int(environ.get('API_ID', '11984338'))
-API_HASH = environ.get('API_HASH', 'ea4cb0f090d7366f7e4ab9dfc116acc7')
-BOT_TOKEN = environ.get('BOT_TOKEN', '5764337430:AAErAYClzy1xB_iH1Up2mN2kEIKGjNqymRY')
+API_ID = int(environ.get('API_ID', '14338'))
+API_HASH = environ.get('API_HASH', 'ea4cb0f090d77e4ab9dfc116acc7')
+BOT_TOKEN = environ.get('BOT_TOKEN', '5764337430:AAErzy1xB_iH1Up2mN2kEIKGjNqymRY')
 FILE_CAPTION = environ.get('FILE_CAPTION', '<code>{file_name}</code>')
-OWNER = int(environ.get('OWNER', '5422115985'))
+OWNER = int(environ.get('OWNER', '54221985'))
 PRIVATE_BOT = bool(environ.get('PRIVATE_BOT', False))
 #==
